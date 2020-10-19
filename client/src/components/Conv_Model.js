07bf141f8aca85d8dae3f11850e5928ec595335f
13jk59 <jesse.khaira10@gmail.com>
2020-10-19 14:07:28 -0600

    added logic to be able to view images that have been uploaded inside the client, along with other html structure that was needed for the conv model

--- client/src/components/Conv_Model.js
+++ client/src/components/Conv_Model.js
@@ -4,24 +4,58 @@ import React from 'react';
 class Conv_Model extends React.Component {
     constructor(props) {
         super(props); 
+
+        this._isImgUploaded = this._isImgUploaded.bind(this); 
+    }
+
+    _isImgUploaded(e) {
+        const imgUploaded = e.target.files[0];
+        const imageDisplayDiv = document.getElementById('imageDisplayDiv');
+        const img = document.getElementById('imgLoaded');
+        const reader = new FileReader();
+
+        reader.onloadend = function() {
+            imageDisplayDiv.style.display = 'block'; 
+            img.src = reader.result; 
+            document.getElementById('submitConv').style.display = 'block'; 
+            document.getElementById('labelPokeImg').style.display = 'none';
+        }
+
+        reader.readAsDataURL(imgUploaded);
     }
     
     render() {
         return(
             <div className = "Model_Div">
                 <div id = "ConvModel">
-                    <form action="/conv_model" method = "post" id = "conv_form">
-                        <label for="pokeImg" id = "labelPokeImg" className="buttonSubmitModel">Submit a .png or .jpg pokemon image like those 
-                        above! </label>
+                    <form action="/conv_model" method = "get" id = "conv_form">
+                        <label for="pokeImg" 
+                        id = "labelPokeImg" 
+                        className="buttonSubmitModel"
+                        onChange = {this._isImgUploaded}>
+
+                            Submit a .png or .jpg pokemon image like those 
+                            above! 
+
+                        </label>
                         <input type="file"
                             id="pokeImg" name="pokeImg"
-                            accept="image/png, image/jpeg">
+                            accept="image/png, image/jpeg"
+                            onChange = {this._isImgUploaded}
+                        >
                         </input>
+
+                        <div id = "imageDisplayDiv">
+                            <p>
+                            This is the image you uploaded. Press submit if you would like to send the
+                            image to the algorithm.
+                            </p>
+                            <img id = "imgLoaded" alt = "Your uploaded image"></img>
+                        </div>
                         <input id = "submitConv" type = "submit" className = "buttonSubmitModel"></input>
-                        <div id = "convResults"></div>
                     </form>
                 </div>
-                <div id = "ResultsLanguageModel"></div>
+                <div id = "convResults"></div>
             </div>        
         );
     }
