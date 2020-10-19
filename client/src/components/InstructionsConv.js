fb15a1afe86206dd2a03402f64e7098c6526ad12
13jk59 <jesse.khaira10@gmail.com>
2020-10-19 12:58:31 -0600

    added instructions to use conv

--- client/src/components/InstructionsConv.js
+++ client/src/components/InstructionsConv.js
@@ -7,7 +7,17 @@ class InstructionsConv extends React.Component {
     }
     render() {
         return(
-            <p>placeholder</p>
+            <div className = "ML_Model_Instructions">
+                <div id = "instructionsConv">
+                    The algorithm was trained to recognize pokémon from the first
+                    150 pokémon. 
+                    
+                    This means you should ideally insert images from those 150 pokémon,
+                    like those shown below. 
+                </div>
+                <div id = "randomPokeImgs">
+                </div>
+            </div>
         );
     }
 }
