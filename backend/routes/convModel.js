a7e5038af1070bf5bac2b871c8e1954a2b5e04ce
13jk59 <jesse.khaira10@gmail.com>
2020-10-20 12:03:06 -0600

    added folder structure for ml model serving in python with flask

--- backend/routes/convModel.js
+++ backend/routes/convModel.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var fileUpload = require('express-fileupload');
 
+
+var fileUpload = require('express-fileupload');
+var Jimp = require('jimp')
 router.use(fileUpload({
     createParentPath: true
 }));
 
-router.post('/', function(req, res) {
-    console.log(req.files); 
-    res.send({response:"placeholder"}); 
+router.post('/', async function(req, res) {
+    const image = await Jimp.read(req.files.image.data);
+    let file = `new_name2.${image.getExtension()}`;
+
+    return image.write(file); 
 });
 
 module.exports = router;
