c33c1d712405e1fbfc63bc64354c83f20b2a9ff5
13jk59 <jesse.khaira10@gmail.com>
2020-10-22 18:43:41 -0600

    updated instructions for using conv model

--- client/src/components/InstructionsConv.js
+++ client/src/components/InstructionsConv.js
@@ -9,7 +9,7 @@ class InstructionsConv extends React.Component {
         return(
             <div className = "ML_Model_Instructions">
                 <div id = "instructionsConv">
-                    To use the algorithm, upload a Pokémon image from the first
+                    To use the algorithm, upload a image of a SINGLE pokémon from the first
                     150 pokémon like those shown below (IE: not sprites), and a prediction will 
                     be returned by the algorithm for which pokémon it thinks it most likely is! 
                 </div>
