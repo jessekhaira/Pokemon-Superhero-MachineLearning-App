5c5723d0e9df8782bf0ba0be3b17786b8fe76cd3
13jk59 <jesse.khaira10@gmail.com>
2020-10-25 16:19:57 -0600

    finalized frontend for the language model

--- client/src/components/LanguageModel.js
+++ client/src/components/LanguageModel.js
@@ -5,8 +5,12 @@ class LanguageModel extends React.Component {
     constructor(props) {
         super(props); 
         this._generateNewName = this._generateNewName.bind(this); 
+        this._resetLM = this._resetLM.bind(this); 
     }
 
+    componentDidMount() {
+        this._setTempLabelInnerHTML(); 
+    }
     async _generateNewName(e) {
         e.preventDefault(); 
         const temperatureVal = document.getElementById('temperatureVal').value;
@@ -19,7 +23,7 @@ class LanguageModel extends React.Component {
         const LM_ModelDiv = document.getElementById("LM_ModelDiv");
         const LanguageModelDiv = document.getElementById("temperatureForm");
         try {
-            this.props._hideDisplays(resultsLM, LanguageModelDiv);  
+            this.props._hideDisplays(document.getElementById('temperatureForm')); 
             LM_ModelDiv.appendChild(this.props._addSpinnerAsync());
             let fetchedData = await fetch('/languageModel', {
                 method: 'POST',
@@ -31,6 +35,7 @@ class LanguageModel extends React.Component {
                 }
             });
             let jsonData = await fetchedData.json(); 
+            this.props._showDisplays('block', resultsLM, document.getElementById('generateAgain')); 
             resultsLM.innerHTML = jsonData.predictedName;
         }
         catch(err) {
@@ -38,10 +43,25 @@ class LanguageModel extends React.Component {
         }
 
         finally {
-            this.props._showDisplays('block', resultsLM, LanguageModelDiv);
             LM_ModelDiv.removeChild(LM_ModelDiv.lastChild); 
         }
 
+    }
+
+    _resetLM() {
+        const resultsLM = document.getElementById("ResultsLanguageModel");
+        this.props._showDisplays('flex',document.getElementById('temperatureForm'));
+        this.props._hideDisplays(resultsLM, document.getElementById('generateAgain')); 
+        // reset the temperature val to be nothing, and reset the string in the label to be the original string because 
+        // there may still be an error message displayed there 
+        document.getElementById('temperatureVal').value = '';
+        this._setTempLabelInnerHTML();
+    }
+
+    _setTempLabelInnerHTML() {
+        document.getElementById('tempLabel').innerHTML = `How random do you want your generated names to be? Setting the temperature above 1
+        will cause the predicted names to be more random, while below 1 will cause the predicted names 
+        to be more conservative. The number has to be between 0.5 and 5 though (inclusive)!`; 
     }
 
    
@@ -50,13 +70,11 @@ class LanguageModel extends React.Component {
             <div className = "Model_Div" id = "LM_ModelDiv">
                 <div id = "ResultsLanguageModel"></div>
                 <form id = "temperatureForm">
-                    <label for = "temperature" id = "tempLabel">How random do you want your generated names to be? Setting the temperature above 1
-                    will cause the predicted names to be more random, while below 1 will cause the predicted names 
-                    to be more conservative. The number has to be between 0.5 and 5 though (inclusive)!</label>
+                    <label for = "temperature" id = "tempLabel"></label>
                     <input type = "number" id = "temperatureVal" name = "temperature" min = "0.5" max = "5" step = "any"></input>
                     <input type= "submit" className = "button" id = "submitLM" onClick = {this._generateNewName} value = "Generate New Pokémon Name"></input>
                 </form>
-                <div id = "generateAgain" className = "button">Generate Again</div>
+                <div id = "generateAgain" className = "button" onClick = {this._resetLM}>Generate Again</div>
             </div>
         );
     }
