61a4ad3cb227b6f50c6fc7afe96066602eb72e93
13jk59 <jesse.khaira10@gmail.com>
2020-10-28 12:52:46 -0600

    added outline of formal image scraping code, to be filled out after playground development of code in notebook is complete

---
+++ ML_Model_Serving/convModel/model training/imageScraper.py
@@ -0,0 +1,41 @@
+import selenium
+import os 
+from PIL import Image 
+def image_scraper(queries, img_width = None, img_height = None):
+    """
+    This function scrapes full-sized images when given an input
+    hashtable where the keys are a string representing a query
+    and the values are the number of images of that query to be
+    scraped.
+
+    Args:
+        queries (dict of str:int): HashMap containing mapping between queries and the number
+            of images of each query that should be scraped
+
+        img_width(int, optional): Optional, integer representing width to save every image
+            that is scraped
+
+        img_height(int, optional): Optional, integer representing height to save every image
+            that is scraped
+
+    Returns:
+        None. A new folder is made for each query string, and the images 
+            associated with the query are saved inside of that folder. 
+
+    """ 
+    
+    # dict of str: list[str]
+    urls_queries = getUrls(queries)
+    # save urls as images
+    saveUrlsAsImages(urls_queries, img_width, img_height)
+
+
+
+def get_urls(queries):
+    pass
+
+def save_urls_as_images(urls_queries, img_width, img_height):
+    pass 
+
+
+
