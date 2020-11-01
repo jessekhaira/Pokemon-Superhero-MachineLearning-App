4f0bab9fd83192d8de81ceb87449fca5e78aca41
13jk59 <jesse.khaira10@gmail.com>
2020-11-01 13:23:40 -0700

    finished code to create language model, and moved around functions to improve modularity

--- ML_Model_Serving/languageModel/model training/languageModel.py
+++ ML_Model_Serving/languageModel/model training/languageModel.py
@@ -1,26 +1,12 @@
 import keras 
-import re 
+from keras.layers import Input 
+from keras.layers import Dense 
+from keras.layers import GRU
+from keras import Model
 
-def data_preprocess_pipeline(text):
-    with open('pokemonNames.txt', 'r') as f:
-        data = f.read()
-    # remove spaces 
-    tokenized = text.lower().split('\n')
-    # remove empty spaces
-    tokenized = list(filter(lambda x: x, tokenized))
-    # for sentence in tokenized data
-    for i,sentence in enumerate(tokenized):
-        tokenized[i] = re.sub(r'\s+', '', sentence)
-    return tokenized
-
-def create_poke_maps(text):
-    unique_chars = set(text) 
-    char_to_index = {c:i for i,c in enumerate(unique_chars)}
-    index_to_char = {i:c for i,c in enumerate(unique_chars)}
-    return char_to_index, index_to_char
-
-
-def char_to_one_hot_vector(char, char_to_index):
-    vector = np.zeros((len(char_to_index),1))
-    vector[char_to_index[char]] = 1 
-    return vector
+def create_char_gru_model(batch_size, vocab_dim, hidden_dim):
+    char_input = Input(shape = (vocab_dim,1), batch_size=batch_size)
+    GRU_cell = GRU(hidden_dim)(char_input)
+    softmax_layer = Dense(vocab_dim, activation = 'softmax')(GRU_cell)
+    model = Model(inputs = char_input, outputs = softmax_layer)
+    return model 
---
+++ ML_Model_Serving/languageModel/model training/languageModelUtils.py
@@ -0,0 +1,28 @@
+import re 
+import numpy as np 
+
+def data_preprocess_pipeline(text):
+    with open('pokemonNames.txt', 'r') as f:
+        data = f.read()
+    # remove spaces 
+    tokenized = text.lower().split('\n')
+    # remove empty spaces
+    tokenized = list(filter(lambda x: x, tokenized))
+    # for sentence in tokenized data
+    for i,sentence in enumerate(tokenized):
+        tokenized[i] = re.sub(r'\s+', '', sentence)
+    return tokenized
+
+def create_poke_maps(text):
+    unique_chars = set(text) 
+    char_to_index = {c:i for i,c in enumerate(unique_chars)}
+    index_to_char = {i:c for i,c in enumerate(unique_chars)}
+    return char_to_index, index_to_char
+
+
+def char_to_one_hot_vector(char, char_to_index):
+    vector = np.zeros((len(char_to_index),1))
+    vector[char_to_index[char]] = 1 
+    return vector
+
+
