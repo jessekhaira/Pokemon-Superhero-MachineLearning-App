c64280603bbf6aba7a727206768f2074a38e5999
13jk59 <jesse.khaira10@gmail.com>
2020-11-01 14:04:36 -0700

    finished parts of the language model

--- ML_Model_Serving/languageModel/model training/languageModel.py
+++ ML_Model_Serving/languageModel/model training/languageModel.py
@@ -3,10 +3,22 @@ from keras.layers import Input
 from keras.layers import Dense 
 from keras.layers import GRU
 from keras import Model
+from languageModelUtils import * 
+import agrparse
 
-def create_char_gru_model(batch_size, vocab_dim, hidden_dim):
+def create_char_gru_model(batch_size, vocab_dim, hidden_dim, learning_rate):
     char_input = Input(shape = (vocab_dim,1), batch_size=batch_size)
     GRU_cell = GRU(hidden_dim)(char_input)
     softmax_layer = Dense(vocab_dim, activation = 'softmax')(GRU_cell)
     model = Model(inputs = char_input, outputs = softmax_layer)
+    loss = keras.losses.CategoricalCrossentropy()
+    optimizer = keras.optimizers.Adam(
+        learning_rate = learning_rate
+    )
+    model.compile(
+        loss = loss,
+        optimizer = optimizer
+    )
     return model 
+
+
