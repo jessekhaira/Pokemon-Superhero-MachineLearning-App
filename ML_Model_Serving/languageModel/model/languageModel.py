51e3d2a82caa4ce03e362247b53004d494830936
13jk59 <jesse.khaira10@gmail.com>
2020-11-03 17:24:03 -0700

    restructured code, re-did imports for language model to make it cleaner, added code to load in all the jsonified items

--- ML_Model_Serving/languageModel/model/languageModel.py
+++ ML_Model_Serving/languageModel/model/languageModel.py
@@ -1,13 +1,14 @@
-import keras 
-from keras.layers import Input 
-from keras.layers import Dense 
-from keras.layers import GRU
-from keras.callbacks import Callback
-from keras.callbacks import ModelCheckpoint
-from keras import Model
-from languageModelUtils import * 
 import os 
+import json
 import argparse
+import numpy as np
+import tensorflow as tf 
+from keras import Model
+from keras.optimizers import Adam
+from keras.losses import SparseCategoricalCrossentropy
+from keras.layers import Input, Dense, GRU, TimeDistributed
+from keras.callbacks import Callback, ModelCheckpoint
+from languageModelUtils import BatchGenerator, char_to_one_hot_vector
 
 
 def create_char_gru_model(vocab_dim, hidden_dim, learning_rate):
@@ -17,13 +18,13 @@ def create_char_gru_model(vocab_dim, hidden_dim, learning_rate):
     """ 
     char_input = Input(shape = (None, vocab_dim), batch_size = 1)
     GRU_cell = GRU(hidden_dim, return_sequences=True)(char_input)
-    predicted_char = keras.layers.TimeDistributed(Dense(vocab_dim))(GRU_cell)
+    predicted_char = TimeDistributed(Dense(vocab_dim))(GRU_cell)
     model = Model(inputs = char_input, outputs = predicted_char)
 
-    optimizer = keras.optimizers.Adam(
+    optimizer = Adam(
         learning_rate = learning_rate
     )
-    loss = keras.losses.SparseCategoricalCrossentropy(
+    loss = SparseCategoricalCrossentropy(
         from_logits=True
     )
     model.compile(
@@ -45,7 +46,7 @@ def make_name(model, index_to_char, char_to_index, temperature=1, min_seq_len = 
         raw_logits = (model.predict(x)[0])/temperature
         # get generated char at this timestep 
         predicted_index = tf.random.categorical(raw_logits, num_samples=1).numpy()[0][0]
-        gen_char = index_to_char[predicted_index]
+        gen_char = index_to_char[str(predicted_index)]
         # append char to name we're making, and create a new vector to be input at the
         # next timestep
         generated_name.append(gen_char)
@@ -64,7 +65,7 @@ class train_callback_generate_names(Callback):
         self.char_to_index = char_to_index
     
     def on_epoch_end(self, epoch, logs):
-        if epoch % 27 == 0:
+        if epoch % 5 == 0:
             print('\n')
             for i in range(5):
                 generated_name = make_name(self.model, self.index_to_char, self.char_to_index)
@@ -73,7 +74,15 @@ class train_callback_generate_names(Callback):
 
 
 def main(vocab_dim, hidden_dim, learning_rate, epochs=10):
-    tokenized_data, char_to_index, index_to_char = get_tokenized_data_get_maps() 
+    with open(os.path.abspath('') +'/jsonified_items/jsonified_preprocessed_data.json', 'r') as f:
+        tokenized_data = json.load(f) 
+    
+    with open(os.path.abspath('') +'/jsonified_items/jsonified_char_to_index.json', 'r') as f:
+        char_to_index = json.load(f) 
+
+    with open(os.path.abspath('') +'/jsonified_items/jsonified_index_to_char.json', 'r') as f:
+        index_to_char = json.load(f)
+
     model = create_char_gru_model(vocab_dim, hidden_dim, learning_rate)
 
     genNamesDuringTraining = train_callback_generate_names(model, index_to_char, char_to_index)
@@ -110,6 +119,7 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+
 
     main(args.vocab_dim, args.hidden_dim, args.learning_rate, args.epochs)
 
