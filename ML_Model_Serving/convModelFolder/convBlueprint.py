82347f956cfc979a0a12afd0559c5c430c0e3f77
13jk59 <jesse.khaira10@gmail.com>
2020-11-08 10:55:22 -0700

    loaded in model and created map from int to string label

--- ML_Model_Serving/convModelFolder/convBlueprint.py
+++ ML_Model_Serving/convModelFolder/convBlueprint.py
@@ -2,12 +2,17 @@ from flask import Blueprint
 from flask import jsonify
 from keras.models import load_model
 import os 
-with open(os.path.join(os.path.abspath(''), 'convModelFolder/model/trained_models/model.50-0.21.h5')) as f:
-    model = load_model(f)
 
-
-convBlueprint = Blueprint('Convolutional Blueprint',__name__)
+convBlueprint = Blueprint('Convolutional Blueprint', __name__)
+model = load_model(os.path.join(os.path.abspath(''), 'convModelFolder/model/trained_models/model.50-0.21.h5'))
+class_indices = {
+    'batman': 0, 
+    'hulk': 1, 
+    'spiderman': 2, 
+    'superman': 3
+}
 @convBlueprint.route('/', methods = ['POST'])
 def convModel_APIHandler(): 
+
     return jsonify({'MostLikelyClass': 'Batman', 'allProbs': {'Batman':0.52, 'Spiderman': 0.21, 'Black Panther': 0.21, 'Wolverine': 0.06}})
 
