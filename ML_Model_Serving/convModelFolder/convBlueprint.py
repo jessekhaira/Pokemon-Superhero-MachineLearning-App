1e0c5cbfa4612da6fff592382d61d6b7fe52050c
13jk59 <jesse.khaira10@gmail.com>
2020-11-08 10:43:39 -0700

    deleted models that were saved that are not going to be used, started working on deploying model at endpoint

--- ML_Model_Serving/convModelFolder/convBlueprint.py
+++ ML_Model_Serving/convModelFolder/convBlueprint.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
 from flask import jsonify
+from keras.models import load_model
+import os 
+with open(os.path.join(os.path.abspath(''), 'convModelFolder/model/trained_models/model.50-0.21.h5')) as f:
+    model = load_model(f)
+
 
 convBlueprint = Blueprint('Convolutional Blueprint',__name__)
 @convBlueprint.route('/', methods = ['POST'])
