b8868a0b0995e3f45b685727c2abe88478021d5d
13jk59 <jesse.khaira10@gmail.com>
2020-11-12 17:18:53 -0700

    updated setup.py to have the package name correct

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='machine learning models api', version='1.0', packages=find_packages())
+setup(name='ML_Model_Serving', version='1.0', packages=find_packages())
