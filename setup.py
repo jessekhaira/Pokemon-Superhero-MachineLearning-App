bd097f09fd0d21df069ff92822fc95f256d0ff17
13jk59 <jesse.khaira10@gmail.com>
2020-11-13 17:26:43 -0700

    removed comments from setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ import os
 with open(os.path.join(os.path.abspath(''), 'ML_Model_Serving/requirements.txt')) as f:
     all_reqs = f.read()
     list_all_reqs = all_reqs.split('\n')
-    # empty string is present at last index, popping removes it 
     list_all_reqs.pop() 
-    # remove all white spaces at beginning of string and ignore -e git+
     install_requires = [string_requirement.strip() for string_requirement in list_all_reqs if 'git+' not in string_requirement]
 
 setup(
