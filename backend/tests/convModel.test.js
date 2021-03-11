0fcb0f8be880660f5f333e9498c6ed1b92108cf4
13jk59 <jesse.khaira10@gmail.com>
2021-03-10 18:25:13 -0700

    grouped tests under describe statement

--- backend/tests/convModel.test.js
+++ backend/tests/convModel.test.js
@@ -3,13 +3,13 @@ const request = require('supertest');
 const fs = require('fs');
 const path = require('path');
 
-test('test POST endpoint located at /convModel', async (done) => {
+describe('group of tests testing /convModel POST endpoint', () => {
+    test('test POST endpoint located at /convModel', async (done) => {
+        let results = await request(app)
+            .post('/convModel')
+            .attach('image', path.resolve(__dirname, 'test_image2.png'));
 
-
-    let results = await request(app)
-        .post('/convModel')
-        .attach('image', path.resolve(__dirname, 'test_image2.png'));
-
-    console.log(results);
-    done(); 
-})
+        console.log(results);
+        done(); 
+    })
+}); 
