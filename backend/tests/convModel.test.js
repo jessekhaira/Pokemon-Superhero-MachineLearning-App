6f89fa7ce4f1ea595d3f30171a865e31c9184752
13jk59 <jesse.khaira10@gmail.com>
2021-03-11 14:44:28 -0700

    added integration test for convModel endpoint

--- backend/tests/convModel.test.js
+++ backend/tests/convModel.test.js
@@ -4,11 +4,17 @@ const path = require('path');
 
 describe('group of tests testing /convModel POST endpoint', () => {
     test('test POST endpoint located at /convModel', async (done) => {
-        let results = await request(app)
+        let results = (await request(app)
             .post('/convModel')
-            .attach('image', path.resolve(__dirname, 'test_image2.png'));
+            .attach('image', path.resolve(__dirname, 'test_image1.jpg'))
+            .expect(201)
+            .expect('Content-Type', /json/)).body;
 
-        console.log(results);
+        
+        expect(results).toHaveProperty('MostLikelyClass');
+        expect(results).toHaveProperty('allProbs');
+
+        expect(results.allProbs.Batman).toBeGreaterThan(0.85)
         done(); 
     })
 }); 
