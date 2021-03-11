c712f52ca8fd554612fa724b69ae97bcacfed4ea
13jk59 <jesse.khaira10@gmail.com>
2021-03-11 14:46:34 -0700

    added new integration test testing convModel endpoint express

--- backend/tests/convModel.test.js
+++ backend/tests/convModel.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const path = require('path');
 
 describe('group of tests testing /convModel POST endpoint', () => {
-    test('test POST endpoint located at /convModel', async (done) => {
+    test('test POST endpoint located at /convModel using test image 1', async (done) => {
         let results = (await request(app)
             .post('/convModel')
             .attach('image', path.resolve(__dirname, 'test_image1.jpg'))
@@ -15,6 +15,21 @@ describe('group of tests testing /convModel POST endpoint', () => {
         expect(results).toHaveProperty('allProbs');
 
         expect(results.allProbs.Batman).toBeGreaterThan(0.85)
+        done(); 
+    })
+
+    test('test POST endpoint located at /convModel using test image 2', async(done) => {
+        let results = (await request(app)
+            .post('/convModel')
+            .attach('image', path.resolve(__dirname, 'test_image2.png'))
+            .expect(201)
+            .expect('Content-Type', /json/)).body;
+
+    
+        expect(results).toHaveProperty('MostLikelyClass');
+        expect(results).toHaveProperty('allProbs');
+
+        expect(results.allProbs.Superman).toBeGreaterThan(0.85)
         done(); 
     })
 }); 
