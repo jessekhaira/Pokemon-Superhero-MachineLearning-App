a1ce1659f8f463984b4dab32fa7721544e5dacfe
13jk59 <jesse.khaira10@gmail.com>
2021-04-01 17:08:54 -0600

    added another test

--- client/src/tests/component-tests/test-conv-model.test.js
+++ client/src/tests/component-tests/test-conv-model.test.js
@@ -47,16 +47,15 @@ describe('testing onClick event handler on div with ID submitConv', () => {
         else {
             await waitFor(() => expect(screen.getByText('See Top Prediction')).toBeVisible()); 
         }
+        expect(screen.getByText(/Upload a .png or .jpeg/)).not.toBeVisible(); 
 
     }
     test(`testing error case for handler -- error button should be shown and we should be able to go back to 
-        original view before uploading an image`,
+        view that was present before uploading an image`,
 
         async () => {
 
             await setUpClickHandlerTestSubmitConv(true);
-
-            expect(screen.getByText(/Upload a .png or .jpeg/)).not.toBeVisible(); 
             
             userEvent.click(screen.getByText(/There was an error/));
 
@@ -67,5 +66,21 @@ describe('testing onClick event handler on div with ID submitConv', () => {
             expect(screen.getByText(/Upload a .png or .jpeg/)).toBeVisible(); 
 
     });
+
+    test(`testing success case for onclick handler -- clicking on see top prediction button`, async () => {
+        await setUpClickHandlerTestSubmitConv();
+
+        userEvent.click(screen.getByText('See Top Prediction'));
+
+        expect(screen.getByText('See Top Prediction')).not.toBeVisible();
+        expect(screen.getByText(/With a probability of 70.0%, the AI says/)).toBeVisible(); 
+        expect(screen.getByText(/Go Back/)).toBeVisible(); 
+
+        userEvent.click(screen.getByText(/Go Back/));
+
+        expect(screen.getByText('See Top Prediction')).toBeVisible();
+        expect(screen.getByText(/With a probability of 70.0%, the AI says/)).not.toBeVisible(); 
+
+    })
 
 }); 
