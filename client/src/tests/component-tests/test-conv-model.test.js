26a2bb9890594a577761c1224f1721f6dedf1725
13jk59 <jesse.khaira10@gmail.com>
2021-03-31 23:55:04 -0600

    updated test for onchange handler

--- client/src/tests/component-tests/test-conv-model.test.js
+++ client/src/tests/component-tests/test-conv-model.test.js
@@ -10,8 +10,16 @@ let imageInputTag;
 test('testing onChange event handler on input tag -- uploading an image', async () => {
     file = new File(['(⌐□_□)'], 'test_image1.jpg', { type: 'image/jpg' });
     imageInputTag = screen.getByLabelText(/Submit a .png or .jpg/);
+
+    // testing on change handler on input tag pokeImg -- before image is uploaded, then after image is uploaded
     expect(screen.getByText(/This is the image you uploaded/)).not.toBeVisible(); 
+    expect(screen.getByText('Submit')).not.toBeVisible(); 
+    expect(screen.getByAltText('Your uploaded image')).not.toHaveAttribute('src');
+
     userEvent.upload(imageInputTag, file);
-    // image gets uploaded, we should be able to see text indicating that image uploaded succesfully
+
     await waitFor(() => expect(screen.getByText(/This is the image you uploaded/)).toBeVisible());
+    expect(screen.getByText('Submit')).toBeVisible(); 
+    expect(screen.getByAltText('Your uploaded image')).toHaveAttribute('src');
+
 });
