d8885775e53edd1f9728c1ea09daeeceb1d50799
13jk59 <jesse.khaira10@gmail.com>
2021-04-03 17:24:43 -0600

    added a file to test server errors

---
+++ client/src/tests/component-tests/test-server-errors.test.js
@@ -0,0 +1,29 @@
+import {screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {setupAppForTests} from '../tests-setup/setup-app';
+
+setupAppForTests(false);
+
+
+test(`testing error case for Conv Model component-- error button should be shown and we should be able to go back to 
+view that was present before uploading an image`,
+    async () => {
+        let file = new File(['(⌐□_□)'], 'test_image1.jpg', { type: 'image/jpg' });
+        let imageInputTag = screen.getByLabelText(/Submit a .png or .jpg/);
+
+        userEvent.upload(imageInputTag, file);
+        await waitFor(() => expect(screen.getByText('Submit')).toBeVisible());
+        userEvent.click(screen.getByText('Submit'));
+
+        await waitFor(() => expect(screen.getByText(/There was an error/)).toBeVisible()); 
+
+        userEvent.click(screen.getByText(/There was an error/));
+
+        expect(screen.getByText('Try new image')).toBeVisible(); 
+
+        userEvent.click(screen.getByText('Try new image'));
+
+        expect(screen.getByText(/Upload a .png or .jpeg/)).toBeVisible(); 
+
+    }
+);
