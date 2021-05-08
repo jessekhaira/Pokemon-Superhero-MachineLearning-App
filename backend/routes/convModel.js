b8dcfcde46d6a99a7754a86620a822a747fd93f5
13jk59 <jesse.khaira10@gmail.com>
2021-05-08 13:30:03 -0600

    refactored code to make general-routes.test.js pass, app.get goes first

--- backend/routes/convModel.js
+++ backend/routes/convModel.js
@@ -1,33 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fetch = require('node-fetch'); 
-var fileUpload = require('express-fileupload');
-var FormData = require('form-data');
-require('dotenv').config();
+var fetch = require("node-fetch");
+var fileUpload = require("express-fileupload");
+var FormData = require("form-data");
+require("dotenv").config();
 
-router.use(fileUpload({
-    createParentPath: true
-}));
-
+router.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
 /**
  * This endpoint responds to POST requests to the /convModel endpoint of the server. This endpoint expects the
- * request body to contain a file containing a jpg or png image, which is then sent to the flask server. 
+ * request body to contain a file containing a jpg or png image, which is then sent to the flask server.
  */
-router.post('/', async function(req, res) {
-    try {
-        var form = new FormData();
-        form.append('image_data_buffer', req.files.image.data.toString('base64'));
-        let recievedData = await fetch(process.env.ML_Server + '/convModel/', {
-            method: "POST",
-            body: form
-        });
-        let jsonData = await recievedData.json(); 
-        res.status(201).json(jsonData); 
-    }
-    catch (err) {
-        res.status(500).json({'message':err.message});
-    }
+router.post("/", async function (req, res) {
+  try {
+    var form = new FormData();
+    form.append("image_data_buffer", req.files.image.data.toString("base64"));
+    let recievedData = await fetch(process.env.ML_Server + "/convModel/", {
+      method: "POST",
+      body: form,
+    });
+    let jsonData = await recievedData.json();
+    res.status(201).json(jsonData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
--- backend/routes/languageModel.js
+++ backend/routes/languageModel.js
@@ -1,31 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fetch = require('node-fetch'); 
-require('dotenv').config();
+var fetch = require("node-fetch");
+require("dotenv").config();
 
 /**
  * This endpoint responds to POST requests to the /languageModel endpoint of the server. This endpoint expects the
  * request body to contain two Numbers: a value for the temperature, and a value for the number of names to generate.
- * These values are then included in a request to the Flask server. 
+ * These values are then included in a request to the Flask server.
  */
-router.post('/', async function(req, res) {
-    try {
-        let recievedData = await fetch(process.env.ML_Server + '/languageModel', {
-            method: 'POST',
-            body: JSON.stringify({
-                temperature: req.body.temperature,
-                number_to_generate: req.body.number_to_generate
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            }
-        });
-        let jsonData = await recievedData.json(); 
-        res.send(jsonData); 
-    }
-    catch(err) {
-        res.status(500).json({message: err.message});
-    }
+router.post("/", async function (req, res) {
+  try {
+    let recievedData = await fetch(process.env.ML_Server + "/languageModel", {
+      method: "POST",
+      body: JSON.stringify({
+        temperature: req.body.temperature,
+        number_to_generate: req.body.number_to_generate,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    let jsonData = await recievedData.json();
+    res.send(jsonData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
