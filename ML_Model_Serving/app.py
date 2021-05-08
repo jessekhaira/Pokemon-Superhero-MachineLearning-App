efd6012322748911c19b8ef0b9ade5e444411138
13jk59 <jesse.khaira10@gmail.com>
2021-05-08 12:39:00 -0600

    cleaned up app.py, added pylintrc following google code style

--- ML_Model_Serving/app.py
+++ ML_Model_Serving/app.py
@@ -4,12 +4,18 @@ from languageModelFolder.languageModelBlueprint import languageModelBlueprint
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
-def rootResponse():
-    return jsonify({'msg': 
-    'Try sending a HTTP POST request to the /convModel endpoint with a RGB image attachment, or a GET request to /languageModel endpoint'})
-app.register_blueprint(convBlueprint, url_prefix = '/convModel')
-app.register_blueprint(languageModelBlueprint, url_prefix = '/languageModel')
+
+@app.route('/', methods=['GET'])
+def root_response():
+    return jsonify({
+        'msg':
+        'Try sending a HTTP POST request to the /convModel endpoint with\
+        a RGB image attachment, or a GET request to /languageModel endpoint'
+    })
+
+
+app.register_blueprint(convBlueprint, url_prefix='/convModel')
+app.register_blueprint(languageModelBlueprint, url_prefix='/languageModel')
 
 if __name__ == "__main__":
-    app.run() 
+    app.run()
