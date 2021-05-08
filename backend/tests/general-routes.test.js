0dd302a574146bb302405e5eddb3d8d2b2fc1d7f
13jk59 <jesse.khaira10@gmail.com>
2021-05-08 13:09:54 -0600

    cleaned up testing file

--- backend/tests/general-routes.test.js
+++ backend/tests/general-routes.test.js
@@ -1,30 +1,27 @@
-const app = require('../app');
-const request = require('supertest');
+const app = require("../app");
+const request = require("supertest");
 
 test(`testing that static assets are returned appropriately -- test1`, async () => {
-    let results = (await request(app)
-        .get('/conv')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=UTF-8'));
+  await request(app)
+    .get("/conv")
+    .expect(200)
+    .expect("Content-Type", "text/html; charset=UTF-8");
 });
 
 test(`testing that static assets are returned appropriately -- test2`, async () => {
-    let results = (await request(app)
-        .get('/convModel')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=UTF-8'));
+  await request(app)
+    .get("/convModel")
+    .expect(200)
+    .expect("Content-Type", "text/html; charset=UTF-8");
 });
 
 test(`testing that static assets are returned appropriately -- test3`, async () => {
-    let results = (await request(app)
-        .get('/languageModel')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=UTF-8'))
+  await request(app)
+    .get("/languageModel")
+    .expect(200)
+    .expect("Content-Type", "text/html; charset=UTF-8");
 });
 
-
 test(`testing that static assets are not found`, async () => {
-    let results = (await request(app)
-        .post('/xhyz')
-        .expect(404));
+  await request(app).post("/xhyz").expect(404);
 });
