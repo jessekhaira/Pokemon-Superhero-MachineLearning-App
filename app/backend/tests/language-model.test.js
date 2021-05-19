7c42830064a62adc8cbeebaa5a8bf4c2f0970bc2
13jk59 <jesse.khaira10@gmail.com>
2021-05-19 16:23:30 -0600

    redid directory structure for better organization + ease of dockerization

---
+++ app/backend/tests/language-model.test.js
@@ -0,0 +1,36 @@
+const app = require('../app');
+const request = require('supertest');
+
+test(`end to end test testing express server & flask server -- this one specifically tests 
+    POST endpoint located at /languageModel for 15 names`, async () => {
+    let results = (
+        await request(app)
+            .post('/languageModel')
+            .send({
+                temperature: 2.5,
+                number_to_generate: 15,
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+    ).body;
+
+    expect(results).toHaveProperty('predictedName');
+    expect(results.predictedName.length).toEqual(15);
+});
+
+test(`end to end test testing express server & flask server -- this one specifically tests 
+    POST endpoint located at /languageModel for 1 name`, async () => {
+    let results = (
+        await request(app)
+            .post('/languageModel')
+            .send({
+                temperature: 2.5,
+                number_to_generate: 1,
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+    ).body;
+
+    expect(results).toHaveProperty('predictedName');
+    expect(results.predictedName.length).toEqual(1);
+});
