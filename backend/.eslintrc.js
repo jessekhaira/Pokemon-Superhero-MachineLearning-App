e7ec0680757f939d5560199242e96c0602f99df2
13jk59 <jesse.khaira10@gmail.com>
2021-05-19 15:24:16 -0600

    added linting + code formatting to the application ++ cleaned up app.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,13 +1,26 @@
 module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {},
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        camelcase: [
+            2,
+            {
+                properties: 'always',
+            },
+        ],
+        'max-len': [
+            2,
+            {
+                code: 80,
+            },
+        ],
+    },
 };
--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,46 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var conv_modelRouter = require("./routes/convModel");
-var languageModelRouter = require("./routes/languageModel");
-var compression = require("compression");
-var helmet = require("helmet");
-require("dotenv").config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var conv_modelRouter = require('./routes/convModel');
+var languageModelRouter = require('./routes/languageModel');
+var compression = require('compression');
+var helmet = require('helmet');
+require('dotenv').config();
 
 var app = express();
 app.use(compression());
 
 app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
+    helmet({
+        contentSecurityPolicy: false,
+    }),
 );
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // find the static css, html, js files from the client
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // since this is a react app, all the views are handled by the frontend
 // so we have our route handlers for the api endpoints our server should provide
 // but if the route doesnt match any of the data endpoints, we just send the html file back and
 // react will handle the routing for the views from there
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // mount the routers that serve the ml models
-app.use("/convModel", conv_modelRouter);
-app.use("/languageModel", languageModelRouter);
+app.use('/convModel', conv_modelRouter);
+app.use('/languageModel', languageModelRouter);
 
 const port = process.env.PORT || 3002;
 
-if (process.env.NODE_ENV !== "TEST") {
-  app.listen(port);
+if (process.env.NODE_ENV !== 'TEST') {
+    app.listen(port);
 }
 
 module.exports = app;
